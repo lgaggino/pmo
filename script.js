8ef5ffa508fc11980e8f65e217b608810b548b6b
lgaggino <90275201+lgaggino@users.noreply.github.com>
2023-07-01 16:44:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-
     /* Cargar archivo json*/
     fetch('data/consolidado.json')
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("HTTP error " + response.status);
+        }
+        return response.json();
+    })
     .then(data => {
+        // Manejo de datos cargados desde el archivo json
 
         document.getElementById('buscador').addEventListener('submit', function(e) {
-            e.preventDefault(); // Evita que la página se recargue al enviar el formulario
+            e.preventDefault(); // Para prevenir que la página se recargue al enviar el formulario
 
             var valorBuscado = document.getElementById('busqueda').value;
 
@@ -16,21 +21,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 return;
             }
 
-            /* Busca el valor en el dataset */
+            /* Buscar el valor en data */
             var resultado = data.filter(function(obj) {
                 return obj.nombre.toLowerCase().includes(valorBuscado.toLowerCase()) || 
                        obj.categoria.toLowerCase().includes(valorBuscado.toLowerCase());
             });
 
-            /* Si se encontró al menos un resultado, muestra el valor de la columna "cobertura" */
+            /* Si se encontró al menos un resultado */
             if (resultado.length > 0) {
+                /* Mostrar el valor de la columna 'cobertura' de cada resultado */
                 var coberturas = resultado.map(function(obj) {
                     return '<p id="p_linea"">'+ obj.nombre +'</p>' +
-                           '<p class="resultado">CategorÍa: ' + obj.categoria + '</p>' +
-                           '<p class="resultado">SubcategorÍa: ' + obj.subcategoria + '</p>' +
-                           '<p class="resultado">Normativa que la incluye: ' + obj.norma + '</p>' +
-                           '<p class="resultado"><b>Nivel de cobertura: ' + (obj.cobertura * 100) + '</b>%</p>' + // pasa a porcentaje
-                           '<p class="resultado">Recomendaciones de uso: ' + obj.recomendaciones + '</p>';
+                    '<p class="resultado">CategorÍa: ' + obj.categoria + '</p>' +
+                    '<p class="resultado">SubcategorÍa: ' + obj.subcategoria + '</p>' +
+                    '<p class="resultado">Normativa que la incluye: ' + obj.norma + '</p>' +
+                    '<p class="resultado"><b>Nivel de cobertura: ' + (obj.cobertura * 100) + '</b>%</p>' + // pasa a porcentaje
+                    '<p class="resultado">Recomendaciones de uso: ' + obj.recomendaciones + '</p>';
                 });
                 document.getElementById('texto-seccion').innerHTML = coberturas.join('<hr>');
                 
@@ -39,5 +45,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
             }
         });
     })
-    .catch(error => console.error('Error:', error));
+    .catch(function() {
+        // Manejo de cualquier error que pueda haber ocurrido en los bloques 'fetch' o 'then' anteriores.
+        console.log("Se produjo un error al cargar el archivo json");
+    });
 });
