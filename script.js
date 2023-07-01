9e7466171e518e7de5e35cbc5a6b21cb550463f4
lgaggino <90275201+lgaggino@users.noreply.github.com>
2023-07-01 18:50:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,70 +1,43 @@
-document.getElementById('excelFile').addEventListener('change', function(e) {
-    var file = e.target.files[0];
-    var reader = new FileReader();
-    reader.onload = function(e) {
-      var jsonContent = e.target.result;
-      var data = JSON.parse(jsonContent);
-  
-      document.getElementById('buscarBtn').addEventListener('click', function(e) {
-        e.preventDefault();
-  
-        var valorBuscado = document.getElementById('busqueda').value;
-        var categoriaSeleccionada = document.getElementById('categoria').value;
-  
-        if (valorBuscado === "" && categoriaSeleccionada === "") {
-          document.getElementById('texto-seccion').innerHTML = '';
-          return;
-        }
-  
-        var resultado = data.filter(function(obj) {
-          if (valorBuscado !== "" && categoriaSeleccionada === "") {
-            return (
-              obj.nombre.toLowerCase().includes(valorBuscado.toLowerCase()) ||
-              obj.categoria.toLowerCase().includes(valorBuscado.toLowerCase())
-            );
-          } else if (valorBuscado === "" && categoriaSeleccionada !== "") {
-            return obj.categoria === categoriaSeleccionada;
-          } else if (valorBuscado !== "" && categoriaSeleccionada !== "") {
-            return (
-              obj.nombre.toLowerCase().includes(valorBuscado.toLowerCase()) &&
-              obj.categoria === categoriaSeleccionada
-            );
-          } else {
-            return false;
-          }
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    /* Cargar archivo json*/
+    fetch('data/consolidado.json')
+    .then(response => response.json())
+    .then(data => {
+
+        document.getElementById('buscador').addEventListener('submit', function(e) {
+            e.preventDefault(); // Evita que la página se recargue al enviar el formulario
+
+            var valorBuscado = document.getElementById('busqueda').value;
+
+            // Si el valor buscado está vacío, no se realiza la búsqueda
+            if (valorBuscado === "") {
+                document.getElementById('texto-seccion').innerHTML = '';
+                return;
+            }
+
+            /* Busca el valor en el dataset */
+            var resultado = data.filter(function(obj) {
+                return obj.nombre.toLowerCase().includes(valorBuscado.toLowerCase()) || 
+                       obj.categoria.toLowerCase().includes(valorBuscado.toLowerCase());
+            });
+
+            /* Si se encontró al menos un resultado, muestra el valor de la columna "cobertura" */
+            if (resultado.length > 0) {
+                var coberturas = resultado.map(function(obj) {
+                    return '<p id="p_linea"">'+ obj.nombre +'</p>' +
+                           '<p class="resultado">CategorÍa: ' + obj.categoria + '</p>' +
+                           '<p class="resultado">SubcategorÍa: ' + obj.subcategoria + '</p>' +
+                           '<p class="resultado">Normativa que la incluye: ' + obj.norma + '</p>' +
+                           '<p class="resultado"><b>Nivel de cobertura: ' + (obj.cobertura * 100) + '</b>%</p>' + // pasa a porcentaje
+                           '<p class="resultado">Recomendaciones de uso: ' + obj.recomendaciones + '</p>';
+                });
+                document.getElementById('texto-seccion').innerHTML = coberturas.join('<hr>');
+                
+            } else {
+                alert('No se encontró el valor buscado');
+            }
         });
-  
-        if (resultado.length > 0) {
-          var coberturas = resultado.map(function(obj) {
-            return (
-              '<p id="p_linea">' +
-              obj.nombre +
-              "</p>" +
-              '<p class="resultado">CategorÍa: ' +
-              obj.categoria +
-              "</p>" +
-              '<p class="resultado">SubcategorÍa: ' +
-              obj.subcategoria +
-              "</p>" +
-              '<p class="resultado">Normativa que la incluye: ' +
-              obj.norma +
-              "</p>" +
-              '<p class="resultado"><b>Nivel de cobertura: ' +
-              obj.cobertura * 100 +
-              "</b>%</p>" +
-              '<p class="resultado">Recomendaciones de uso: ' +
-              obj.recomendaciones +
-              "</p>"
-            );
-          });
-          document.getElementById("texto-seccion").innerHTML = coberturas.join(
-            "<hr>"
-          );
-        } else {
-          alert("No se encontró el valor buscado");
-        }
-      });
-    };
-    reader.readAsText(file);
-  });
-  
+    })
+    .catch(error => console.error('Error:', error));
+});
