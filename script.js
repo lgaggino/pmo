a352c11dc4617131d79b152f533913a860af8ee2
lgaggino <90275201+lgaggino@users.noreply.github.com>
2023-07-02 12:01:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-
-    /* Cargar archivo json*/
+    /* Cargar archivo json */
     fetch('https://lgaggino.github.io/pmo/consolidado.json')
     .then(response => response.json())
     .then(data => {
+        /* Obtener categorías únicas para el desplegable */
+        let categories = [...new Set(data.map(item => item.categoria))];
+        let selectElement = document.getElementById('categoria');
+        categories.forEach(category => {
+            let optionElement = document.createElement('option');
+            optionElement.value = category;
+            optionElement.textContent = category;
+            selectElement.appendChild(optionElement);
+        });
 
         document.getElementById('buscador').addEventListener('submit', function(e) {
             e.preventDefault(); // Evita que la página se recargue al enviar el formulario
 
             var valorBuscado = document.getElementById('busqueda').value;
+            var valorCategoria = document.getElementById('categoria').value;
 
-            // Si el valor buscado está vacío, no se realiza la búsqueda
-            if (valorBuscado === "") {
+            /* Si el valor buscado está vacío, no se realiza la búsqueda */
+            if (valorBuscado === "" && valorCategoria === "") {
                 document.getElementById('texto-seccion').innerHTML = '';
                 return;
             }
 
             /* Busca el valor en el dataset */
             var resultado = data.filter(function(obj) {
-                return obj.nombre.toLowerCase().includes(valorBuscado.toLowerCase()) || 
-                       obj.categoria.toLowerCase().includes(valorBuscado.toLowerCase());
+                return (obj.nombre.toLowerCase().includes(valorBuscado.toLowerCase()) || obj.categoria.toLowerCase().includes(valorBuscado.toLowerCase())) &&
+                       (valorCategoria === "" || obj.categoria === valorCategoria);
             });
 
             /* Si se encontró al menos un resultado, muestra el valor de la columna "cobertura" */
@@ -33,7 +42,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
                            '<p class="resultado">Recomendaciones de uso: ' + obj.recomendaciones + '</p>';
                 });
                 document.getElementById('texto-seccion').innerHTML = coberturas.join('<hr>');
-                
             } else {
                 alert('No se encontró el valor buscado');
             }
