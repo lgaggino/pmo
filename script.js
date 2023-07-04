3cf82b4e577b74ad86fe2658f396d523c4ebf443
lgaggino <90275201+lgaggino@users.noreply.github.com>
2023-07-04 11:10:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
-    /* Cargar archivo json*/
+    /* Cargar archivo json */
     fetch('https://lgaggino.github.io/pmo/consolidado.json')
     .then(response => response.json())
     .then(data => {
+
+        /* Obtener categorías únicas para el desplegable */
+        let categories = [...new Set(data.map(item => item.categoria))];
+        let selectElement = document.getElementById('categoria');
+        categories.forEach(category => {
+            let optionElement = document.createElement('option');
+            optionElement.value = category;
+            optionElement.textContent = category;
+            selectElement.appendChild(optionElement);
+        });
+
+        /* Cuando el campo de búsqueda cambia, vacía el desplegable */
+        document.getElementById('busqueda').addEventListener('input', function(e) {
+            document.getElementById('categoria').value = '';
+        });
+
+        /* Cuando el desplegable cambia, vacía el campo de búsqueda */
+        document.getElementById('categoria').addEventListener('change', function(e) {
+            document.getElementById('busqueda').value = '';
+        });
 
         document.getElementById('buscador').addEventListener('submit', function(e) {
             e.preventDefault(); // Evita que la página se recargue al enviar el formulario
 
             var valorBuscado = document.getElementById('busqueda').value;
-
-            // Si el valor buscado está vacío, no se realiza la búsqueda
-            if (valorBuscado === "") {
-                document.getElementById('texto-seccion').innerHTML = '';
-                return;
-            }
+            var valorCategoria = document.getElementById('categoria').value;
 
             /* Busca el valor en el dataset */
             var resultado = data.filter(function(obj) {
-                return obj.nombre.toLowerCase().includes(valorBuscado.toLowerCase()) || 
-                       obj.categoria.toLowerCase().includes(valorBuscado.toLowerCase());
+                if (valorBuscado !== "") {
+                    return obj.nombre.toLowerCase().includes(valorBuscado.toLowerCase()) || obj.categoria.toLowerCase().includes(valorBuscado.toLowerCase());
+                } else {
+                    return obj.categoria === valorCategoria;
+                }
             });
 
             /* Si se encontró al menos un resultado, muestra el valor de la columna "cobertura" */
             if (resultado.length > 0) {
+                document.getElementById('texto-seccion').style.display = 'block'; // Muestra la sección de resultados
                 var coberturas = resultado.map(function(obj) {
                     return '<p id="p_linea"">'+ obj.nombre +'</p>' +
                            '<p class="resultado">CategorÍa: ' + obj.categoria + '</p>' +
